024b0c03383081c4cf5f04fdd3e79378b15e1872
Andy Freidenfelds <afreidenfelds@gmail.com>
2022-10-05 10:19:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Code Server Font Patch
+
+This patchs [code-server](https://github.com/cdr/code-server) to load Blex Mono Nerd Font.
+
+## Installation
+
+- Clone this repository.
+
+```bash
+git clone https://github.com/afreidz/code-server-blex-patch
+cd code-server-blex-patch
+```
+
+- Run this command (change `path-to-code-server` to your code-server path, leave it empty if you install code-server from installer or code-server is in `/usr/lib/code-server`):
+
+```bash
+sudo ./patch.sh [path-to-code-server]
+```
---
+++ patch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ -z $1 ]; then
+  CODE_SERVER_PATH="/usr/lib/code-server"
+else
+  CODE_SERVER_PATH="$1"
+fi
+
+WORKBENCH_PATH="$CODE_SERVER_PATH/lib/vscode/out/vs/workbench"
+WORKBENCH_CSS_PATH="$WORKBENCH_PATH/workbench.web.main.css"
+
+# Check if it is valid path
+if [ -z "$CODE_SERVER_PATH" -o ! -d "$WORKBENCH_PATH" ]; then
+  echo "Please give valid code-server path!"
+  exit 1
+fi
+
+if ! grep -q "/* ::CUSTOM VSCODE FONTS:: */" "$WORKBENCH_CSS_PATH"; then
+  # Copy fonts to $WORKBENCH_PATH
+  cp -rn ./resources/fonts "$WORKBENCH_PATH/"
+
+  # Prepend fonts.css to workbench.web.api.css
+  cat ./resources/fonts.css >> $WORKBENCH_CSS_PATH
+fi
+
+echo "Done Patching Code Server to include Blex Font!"
